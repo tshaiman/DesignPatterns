c1a8af603c2f7870b6cbc594ab168a043910c330
tshaiman <tomer.shaiman@gmail.com>
2018-08-12 00:27:35 +0300

    dip

---
+++ Solid05-DIP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid05_DIP
+{
+    class Program
+    {
+        //Refactor the entier Commerce application according to the DIP 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
