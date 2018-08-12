2b7cba29c824112c97927e289ce9fd519bc2cec0
msvs <msvs@LESHEM0>
2018-08-12 09:11:41 +0300

    structural

---
+++ Decorator/Beverage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class Beverage
+    {
+        public String Description { get; protected set; }
+
+        public Beverage()
+        {
+            Description = "Unknown Beverage";
+        }
+
+        public abstract double Cost();
+    }
+
+
+    public abstract class BeverageDecorator :Beverage
+    {
+        protected Beverage beverage { get; set; }
+    }
+
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            Description = "Espresso";
+        }
+
+        public override double Cost()
+        {
+            return 0.99;
+        }
+    }
+
+
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            Description = "House Blend";
+        }
+
+        public override double Cost()
+        {
+            return 0.89;
+        }
+    }
+
+    public class Mocha : BeverageDecorator
+    {
+        public Mocha(Beverage beverage)
+        {
+            this.beverage = beverage;
+            this.Description = this.beverage.Description + " , Mocha";
+        }
+
+        public override double Cost()
+        {
+            return 0.2 + beverage.Cost();
+        }
+    }
+
+
+    public class Whip : BeverageDecorator
+    {
+
+        public Whip(Beverage beverage)
+        {
+            this.beverage = beverage;
+            this.Description = beverage.Description + " ,Whip";
+        }
+
+        public override double Cost()
+        {
+            return 0.3 + beverage.Cost();
+        }
+    }
+
+
+
+
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage houseBlend = new HouseBlend();
+            houseBlend = new Mocha(houseBlend);
+            houseBlend = new Mocha(houseBlend);
+            houseBlend = new Whip(houseBlend);
+
+            Console.WriteLine(houseBlend.Description + " : Cost " + houseBlend.Cost());
+        }
+    }
+}
